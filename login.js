2fab805bcea073eac4a75cc71e0e874797bc0997
JasonNguyen2404 <namduy.1989@gmail.com>
2026-04-19 18:28:40 +0700

    sửa log in

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function login() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = document.getElementById("EmailID").value;
+    let password = document.getElementById("Password").value;
     console.log("Username:", username);
     console.log("Password:", password);
     if (username === "admin" && password === "password") {
